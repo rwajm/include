af793b0598533a1e67f18d30a62ed933aefea537
JeeIn-Shin <wldls0530@gmail.com>
2023-02-07 04:05:30 +0900

    파일 구조 정리 및 html과 연결

--- server.js
+++ server.js
@@ -4,24 +4,38 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use('/img',express.static(path.join(__dirname, 'public')));
+
 const memberBoardRouter = require('./router/memberRegister');
 const activityBoardRouter= require('./router/activity');
 
 
 app.set('port', process.env.PORT || 8080);
 
-app.use(express.json());
-var cors = require('cors');
-app.use(cors());
-
-app.use(express.static(path.join(__dirname, 'build')));
-
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//html
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+app.engine('html', require('ejs').__express);
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//react
+// var cors = require('cors');
+// app.use(cors());
+
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/member', memberBoardRouter);
 app.use('/activity', activityBoardRouter);
 
+app.get('/', (req, res) => {
+    res.render('home.html');
+})
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), (req, res) => {
     console.log(app.get('port'), "빈 포트에서 대기");
 });
