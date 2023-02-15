87eed0c574fb6b25c5a8c6dc5404c955e03f97da
JeeIn-Shin <wldls0530@gmail.com>
2023-02-16 01:40:40 +0900

    member 등록, list rendering 부분 수정

--- router/memberRegister.js
+++ router/memberRegister.js
@@ -10,27 +10,35 @@ router.get('/:reservedWord', async(req, res) => {
     //     message : "Not found"
     // }
     
-    // if(isNaN(req.params.reservedWord))  {
-    //     (req.params.reservedWord === 'create') 
-    //     ? res.render('member/create') //혹시 원하는 페이지로 이동하지 않는다면 여길 손봐주세요
-    //     : (req.params.reservedWord === 'list')
-    //     ? res.render('member/list') //혹시 원하는 페이지로 이동하지 않는다면 여길 손봐주세요
-    //     : (req.params.reservedWord === 'update')
-    //     ? res.render('member/update')
-    //     : res.json(result)
-    // }
-    // else    {
-    //     await MEMBER.getByidx(req.params.reservedWord, (err, data) => {
-    //         try {
-    //             res.json(data);
-    //             //res.render('member/detail', {data});
-    //         }
-    //         catch(err) {
-    //             console.error("router error " + err);
-    //             res.json(result);
-    //         }
-    //     })
-    // }
+    if(isNaN(req.params.reservedWord))  {
+        (req.params.reservedWord === 'create') 
+        ? res.render('member/create') //혹시 원하는 페이지로 이동하지 않는다면 여길 손봐주세요
+        : (req.params.reservedWord === 'update')
+        ? res.render('member/update')
+        : (req.params.reservedWord === 'list')
+        ? await MEMBER.getAll((err, data) => {
+            try {
+                res.render('member/list', { data })
+            }
+            catch(err)  {
+                console.error("router error " + err);
+                res.status(503);
+            }
+        })
+        : res.json(result)
+    }
+    else    {
+        await MEMBER.getByidx(req.params.reservedWord, (err, data) => {
+            try {
+                res.json(data);
+                //res.render('member/detail', {data});
+            }
+            catch(err) {
+                console.error("router error " + err);
+                res.json(result);
+            }
+        })
+    }
 
     //react
     let result = {
